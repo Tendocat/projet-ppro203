b8caca2ff773774ef0517478029616aa3e09a90c
Tendocat <48944658+Tendocat@users.noreply.github.com>
2019-04-29 13:32:24 +0200

    préchargement des images
    faut créer un tableau [[tuile[0], (x1,y1)],[tuile[0], (x2,y2)]]

--- mahjong.py
+++ mahjong.py
@@ -1,20 +1,23 @@
 import pygame
 from pygame.locals import *
 
+def chargement_images():
+	tuiles = []
+	for k in range(4):
+		print(k)
+		tuile = pygame.image.load(f"{k}.png").convert()#convert_alpha() pour transparence 
+		#tuile.set_colorkey((255,255,255)) #Rend le blanc (valeur RGB : 255,255,255) de l'image transparent
+		tuiles.append(tuile)
+
+	return tuiles
+
 pygame.init()
 
 #Ouverture de la fenetre Pygame
 fenetre = pygame.display.set_mode((640, 480))
 
-#Chargement et collage du fond
-fond = pygame.image.load("background.jpg").convert()
-fenetre.blit(fond, (0,0))
-
-#Chargement et collage du personnage
-perso = pygame.image.load("perso.png").convert()#convert_alpha() pour transparence 
-perso.set_colorkey((255,255,255)) #Rend le blanc (valeur RGB : 255,255,255) de l'image transparent
-
-fenetre.blit(perso, (200,300))#colle perso sur fenetre
+#Chargement des images de tuiles (tuile = [[pic0][pic1]]
+tuile = chargement_images()
 
 #Rafraichissement de l'ecran
 pygame.display.flip()
@@ -31,6 +34,12 @@ while continuer:
 				print("Espace")
 			if event.key == K_RETURN:
 				print("Entrée")
+	#Re-collage
+	#fenetre.blit(fond, (0,0))
+	fenetre.blit(tuile[0], (0, 0))#def generer tableau(): for k in range(len(tuile)): tuile[k].append((x,y)) // bien (x,y) avec parenthèses
+	#Rafraichissement
+	pygame.display.flip()
+	
 
 #cours complet :
 #https://openclassrooms.com/fr/courses/1399541-interface-graphique-pygame-pour-python/
