5e40dc1dea57a8a38dca0a84b8dc970dcedb9f23
Tendocat <48944658+Tendocat@users.noreply.github.com>
2019-04-30 18:47:07 +0200

    ajout des constantes height et width

--- mahjong.py
+++ mahjong.py
@@ -6,22 +6,21 @@ def chargement_images():
 	tuiles = []
 	for k in range(10):
 		print(k)
-		tuile = pygame.image.load(f"{k}.png").convert_alpha()#convert_alpha() pour transparence 
-		tuile.set_colorkey((255,255,255)) #Rend le blanc (valeur RGB : 255,255,255) de l'image transparent
-		tuiles.append(tuile)
+		t = pygame.image.load(f"{k}.png").convert()
+		tuiles.append(t)
 	return tuiles
 	
 #return genere un tableau avec lignes = [[image0, (x, y)],...,[imageN, (xN, y]] en lisant un fichier contenant les chiffres des images
 #param tuiles : le tableau contenant les images precharges
 #param L, H la largeur et hauteur d'une tuile
-def generation_tab_fichier(tuile, L = 30, H = 60):
+def generation_tab_fichier(tuile):
 	tableau = []
 	fichier = open('tableau', 'r')
-	for y in range(60, 420, H):
+	for y in range(60, 420, HEIGHT):
 		ligne = fichier.readline()
 		col = 0
 		t = []
-		for x in range(30, 600, L):
+		for x in range(30, 600, WIDTH):
 			if col < len(ligne) :
 				if ligne[col] == '0':
 					t.append([tuile[0], (x,y)])
@@ -50,33 +49,36 @@ def generation_tab_fichier(tuile, L = 30, H = 60):
 	
 #affiche le tableau du mahjong
 def refresh(tableau):
-	#Re-collage
-	#fenetre.blit(fond, (0,0))
+	fenetre.blit(fond, (0,0))#ecrase tout avec le fond
 	for ligne in range(len(tableau)):
 		for tuile in tableau[ligne]:
 			fenetre.blit(tuile[0], tuile[1])#0 = image; 1 = coordonnees
 	pygame.display.flip() #Rafraichissement
 
 #test si la position est sur une tuile
-def test(x, y, L = 30, H = 60):
+#return la position (x, y) de la tuile selectionnee
+def test(x, y):
 	selection = 0
 	for bout in tableau:
 		if len(bout) > 0:
 			(a, b) = bout[0][1]
-			if x > a and y > b and x < a + L  and y < b + H:
+			if x > a and y > b and x < a + WIDTH  and y < b + HEIGHT:
 				selection = bout[0]
 			else:
 				(a, b) = bout[len(bout)-1][1]
-				if x > a and y > b and x < a + L  and y < b + H:
+				if x > a and y > b and x < a + WIDTH  and y < b + HEIGHT:
 					selection = bout[len(bout)-1][1]
 	return selection
+	
 pygame.init()
-
-#Ouverture de la fenetre Pygame
-fenetre = pygame.display.set_mode((630, 480))
+fenetre = pygame.display.set_mode((630, 480))#Ouverture de la fenetre Pygame
+fond = pygame.Surface(fenetre.get_size()).convert()#creation du fond a la taille de la fenetre
+fond.fill((100,100,200))#couleur du fond
 
 #Chargement du tableau ([[tuile, (x,y)]]
 tuile = chargement_images()
+(WIDTH, HEIGHT) = tuile[0].get_size()#setup des constantes de la largeur et hauteur des tuiles
+WIDTH , HEIGHT = 30, 60#tant que images pas bonnes
 tableau = generation_tab_fichier(tuile)
 refresh(tableau)
 
